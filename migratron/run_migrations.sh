fbc8d4ebb38845f6180fac2c9260c2a0528b52f4
Dante <dante.pawlow@jampp.com>
2020-11-30 09:52:26 -0300

    Break long lines up in run_migrations.sh

--- migratron/run_migrations.sh
+++ migratron/run_migrations.sh
@@ -6,7 +6,10 @@ source ./migration_paths.sh
 if [ -z "$BASE_SCHEMA_PATH" ]; then
     echo "\$BASE_SCHEMA_PATH is empty."
 else
-    migratron initialize --just-base-schema --state-db-uri "postgres://hive:hive@hive-metastore-postgresql/metastore" --migrations-path $BASE_SCHEMA_PATH
+    migratron initialize \
+        --just-base-schema \
+        --state-db-uri "postgres://hive:hive@hive-metastore-postgresql/metastore" \
+        --migrations-path $BASE_SCHEMA_PATH
 fi
 
 if [ ${#HIVE_SCHEMA_PATHS[@]} -eq 0 ]; then
@@ -15,7 +18,13 @@ else
     echo "Running HIVE migrations..."
     for p in "${HIVE_SCHEMA_PATHS[@]}"
     do
-        migratron migrate --state-db-uri "postgres://hive:hive@hive-metastore-postgresql/metastore" --migrations-path $p --db-type hive --db-uri jdbc:hive2://hive-server:10000/ --migration-type any --batch-mode
+        migratron migrate \
+            --state-db-uri "postgres://hive:hive@hive-metastore-postgresql/metastore" \
+            --migrations-path $p \
+            --db-type hive \
+            --db-uri jdbc:hive2://hive-server:10000/ \
+            --migration-type any \
+            --batch-mode
     done
 fi
 
@@ -25,6 +34,12 @@ else
     echo "Running PRESTO migrations..."
     for p in "${PRESTO_SCHEMA_PATHS[@]}"
     do
-        migratron migrate --state-db-uri "postgres://hive:hive@hive-metastore-postgresql/metastore" --migrations-path $p --db-type presto --db-uri presto-coordinator:8080 --migration-type any --batch-mode
+        migratron migrate \
+            --state-db-uri "postgres://hive:hive@hive-metastore-postgresql/metastore" \
+            --migrations-path $p \
+            --db-type presto \
+            --db-uri presto-coordinator:8080 \
+            --migration-type any \
+            --batch-mode
     done
 fi
