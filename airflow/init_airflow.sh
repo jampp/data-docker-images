74de216cd56891e470ae42b20492dd8b20b5bfc7
Rcharriol <ramirocharriol@gmail.com>
2022-06-07 19:25:42 +0000

    DI-2615: Update so it can be airflow 2 compliant

--- airflow/init_airflow.sh
+++ airflow/init_airflow.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Init db, add pools, variables and connections
-create_connections() {
+
+delete_connections() {
     # Merge default_connections and extra_connections
     connections=`jq -s '.[0] * .[1]' ${CONFIG_FILES_PATH}/default_connections.json ${CONFIG_FILES_PATH}/extra_connections.json`
 
@@ -11,26 +12,14 @@ create_connections() {
     for conn_id in "${conn_ids[@]}"
     do
         # To modify an existing connection it must be deleted and re-added
-        airflow connections -d --conn_id $conn_id
-
-        # Build the command this way to avoid optional parameters
-        cmd="airflow connections -a --conn_id $conn_id"
-
-        connection=`echo $connections | jq .$conn_id `
-        mapfile -t args < <(echo $connection | jq  -r 'keys[]')
-
-        for arg in "${args[@]}"
-        do
-            cmd+=" --$arg "
-            value=`echo $connection | jq .$arg`
-            cmd+="$value"
-        done
-
-        eval $cmd
+        airflow connections delete $conn_id
     done
 }
 
-airflow initdb
-airflow variables -i "${CONFIG_FILES_PATH}/variables.json"
-airflow pool -i "${CONFIG_FILES_PATH}/pools.json"
-create_connections
+
+airflow db init
+airflow variables import "${CONFIG_FILES_PATH}/variables.json"
+airflow pools import "${CONFIG_FILES_PATH}/pools.json"
+delete_connections
+airflow connections import "${CONFIG_FILES_PATH}/extra_connections.json"
+airflow connections import "${CONFIG_FILES_PATH}/default_connections.json"
